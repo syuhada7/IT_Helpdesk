d354b5df58a2fdea7914b0dd99ec43c2145e1bb5
syuhada7 <nurjuliadisyuhada@gmail.com>
2022-09-13 11:37:14 +0700

    eddit profile user

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -7,12 +7,21 @@ class Users extends CI_Controller
   {
     parent::__construct();
     check_not_login();
-    $this->load->model('User_model');
+    $this->load->model(['User_model', 'Helpdesk_model']);
     $this->load->library('form_validation');
   }
   public function index()
   {
     // $data['row'] = $this->User_model->get();
-    $this->template->load('templates/template', 'Users/index');
+    $name = $this->fungsi->user_login()->username;
+    $data['row'] = $this->Helpdesk_model->getDash($name)->result();
+    $this->template->load('templates/template', 'Users/index', $data);
+  }
+
+  function users()
+  {
+    $name = $this->fungsi->user_login()->username;
+    $data['row'] = $this->Helpdesk_model->getDash($name)->result();
+    $this->template->load('templates/template', 'users/list', $data);
   }
 }
